236ac8d9c42d126b826914d00f954db3be869c37
Menghan Li <menghanl@google.com>
2020-03-23 18:41:38 -0700

    [xds_v2client_multiple_watches_v2client] add copyright

--- xds/internal/client/v2client_eds_testutil.go
+++ xds/internal/client/v2client_eds_testutil.go
@@ -13,6 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 // All structs/functions in this file should be unexported. They are used in EDS
