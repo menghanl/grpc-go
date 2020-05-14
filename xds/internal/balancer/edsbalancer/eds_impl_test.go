c2ac1072dc02e8bb3454f82f62868062313b3d40
Menghan Li <menghanl@google.com>
2020-05-14 11:35:11 -0700

    [xds_error_handling_new] eds balancer tests

--- xds/internal/balancer/edsbalancer/eds_impl_test.go
+++ xds/internal/balancer/edsbalancer/eds_impl_test.go
@@ -18,6 +18,7 @@ package edsbalancer
 
 import (
 	"fmt"
+	"reflect"
 	"sort"
 	"testing"
 	"time"
@@ -386,53 +387,76 @@ func (s) TestClose(t *testing.T) {
 	edsb.close()
 }
 
-// // TestEDS_EmptyUpdate covers the cases when eds impl receives an empty update.
-// //
-// // It should send an error picker with transient failure to the parent.
-// //
-// // TODO: currently the error is always balancer.TransientError. When updated to
-// // new balancer API, it should return error in type Transient Failure, but with
-// // better error details (all sub-balancer removed).
-// //
-// // TODO: if the first update is an empty update, the picker won't be updated
-// // because the update will just be ignored. This can be fixed in two ways:
-// // 1. eds explicitly updates picker to be an error picker when it receives an
-// // empty update
-// // 2. the parent of eds closes the eds balancer, and explicitly updates the
-// // picker.
-// // This needs to be fixed when the decision is made.
-// func (s) TestEDS_EmptyUpdate(t *testing.T) {
-// 	cc := newTestClientConn(t)
-// 	edsb := newEDSBalancerImpl(cc, nil)
+// TestEDS_EmptyUpdate covers the cases when eds impl receives an empty update.
 //
-// 	// One locality with one backend.
-// 	clab1 := xdsclient.NewClusterLoadAssignmentBuilder(testClusterNames[0], nil)
-// 	clab1.AddLocality(testSubZones[0], 1, 0, testEndpointAddrs[:1], nil)
-// 	edsb.HandleEDSResponse(xdsclient.ParseEDSRespProtoForTesting(clab1.Build()))
+// It should send an error picker with transient failure to the parent.
 //
-// 	sc1 := <-cc.newSubConnCh
-// 	edsb.HandleSubConnStateChange(sc1, connectivity.Connecting)
-// 	edsb.HandleSubConnStateChange(sc1, connectivity.Ready)
-//
-// 	// Pick with only the first backend.
-// 	p1 := <-cc.newPickerCh
-// 	for i := 0; i < 5; i++ {
-// 		gotSCSt, _ := p1.Pick(balancer.PickInfo{})
-// 		if !reflect.DeepEqual(gotSCSt.SubConn, sc1) {
-// 			t.Fatalf("picker.Pick, got %v, want SubConn=%v", gotSCSt, sc1)
-// 		}
-// 	}
-//
-// 	edsb.HandleEDSResponse(&xdsclient.EDSUpdate{})
-// 	// Pick should fail with transient failure.
-// 	perr1 := <-cc.newPickerCh
-// 	for i := 0; i < 5; i++ {
-// 		_, err := perr1.Pick(balancer.PickInfo{})
-// 		if !reflect.DeepEqual(err, balancer.ErrTransientFailure) {
-// 			t.Fatalf("picker.Pick, got error %v, want error %v", err, balancer.ErrTransientFailure)
-// 		}
-// 	}
-// }
+// TODO: if the first update is an empty update, the picker won't be updated
+// because the update will just be ignored. This can be fixed in two ways:
+// 1. eds explicitly updates picker to be an error picker when it receives an
+// empty update
+// 2. the parent of eds closes the eds balancer, and explicitly updates the
+// picker.
+// This needs to be fixed when the decision is made.
+func (s) TestEDS_EmptyUpdate(t *testing.T) {
+	cc := testutils.NewTestClientConn(t)
+	edsb := newEDSBalancerImpl(cc, nil, nil, nil)
+	edsb.enqueueChildBalancerStateUpdate = edsb.updateState
+
+	edsb.handleEDSResponse(xdsclient.EndpointsUpdate{})
+	// Pick should fail with transient failure, and all priority removed error.
+	perr0 := <-cc.NewPickerCh
+	for i := 0; i < 5; i++ {
+		_, err := perr0.Pick(balancer.PickInfo{})
+		if !reflect.DeepEqual(err, errAllPrioritiesRemoved) {
+			t.Fatalf("picker.Pick, got error %v, want error %v", err, errAllPrioritiesRemoved)
+		}
+	}
+
+	// One locality with one backend.
+	clab1 := xdsclient.NewClusterLoadAssignmentBuilder(testClusterNames[0], nil)
+	clab1.AddLocality(testSubZones[0], 1, 0, testEndpointAddrs[:1], nil)
+	edsb.handleEDSResponse(xdsclient.ParseEDSRespProtoForTesting(clab1.Build()))
+
+	sc1 := <-cc.NewSubConnCh
+	edsb.handleSubConnStateChange(sc1, connectivity.Connecting)
+	edsb.handleSubConnStateChange(sc1, connectivity.Ready)
+
+	// Pick with only the first backend.
+	p1 := <-cc.NewPickerCh
+	for i := 0; i < 5; i++ {
+		gotSCSt, _ := p1.Pick(balancer.PickInfo{})
+		if !reflect.DeepEqual(gotSCSt.SubConn, sc1) {
+			t.Fatalf("picker.Pick, got %v, want SubConn=%v", gotSCSt, sc1)
+		}
+	}
+
+	edsb.handleEDSResponse(xdsclient.EndpointsUpdate{})
+	// Pick should fail with transient failure, and all priority removed error.
+	perr1 := <-cc.NewPickerCh
+	for i := 0; i < 5; i++ {
+		_, err := perr1.Pick(balancer.PickInfo{})
+		if !reflect.DeepEqual(err, errAllPrioritiesRemoved) {
+			t.Fatalf("picker.Pick, got error %v, want error %v", err, errAllPrioritiesRemoved)
+		}
+	}
+
+	// Handle another update with priorities and localities.
+	edsb.handleEDSResponse(xdsclient.ParseEDSRespProtoForTesting(clab1.Build()))
+
+	sc2 := <-cc.NewSubConnCh
+	edsb.handleSubConnStateChange(sc2, connectivity.Connecting)
+	edsb.handleSubConnStateChange(sc2, connectivity.Ready)
+
+	// Pick with only the first backend.
+	p2 := <-cc.NewPickerCh
+	for i := 0; i < 5; i++ {
+		gotSCSt, _ := p2.Pick(balancer.PickInfo{})
+		if !reflect.DeepEqual(gotSCSt.SubConn, sc2) {
+			t.Fatalf("picker.Pick, got %v, want SubConn=%v", gotSCSt, sc2)
+		}
+	}
+}
 
 // Create XDS balancer, and update sub-balancer before handling eds responses.
 // Then switch between round-robin and test-const-balancer after handling first
