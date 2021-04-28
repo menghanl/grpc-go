3e5e70c4c86999e12825a2b0e9bfb651df1814a1
Menghan Li <menghanl@google.com>
2021-04-28 11:20:41 -0700

    [swap_eds_to_split_8] eds: rewrite
    This change rewrites the EDS balancer to make use of child policies
    (priority, cluster_impl, weighted_target, etc.) instead of doing
    everything on its own.
    
    The new EDS policy is very similar to the CDS balancer (both
    functionality-wise and code-wise). It watches service config updates,
    builds child policies (by building the json config), and passes on the
    other ClientConn updates.

---
+++ xds/internal/balancer/edsbalancer/eds_watcher.go
@@ -0,0 +1,84 @@
+/*
+ *
+ * Copyright 2021 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package edsbalancer
+
+import (
+	"sync"
+
+	"google.golang.org/grpc/internal/grpclog"
+	xdsclient "google.golang.org/grpc/xds/internal/client"
+)
+
+// edsWatcher takes an EDS balancer config, and use the xds_client to watch EDS
+// updates. The EDS updates are passed back to the balancer via a channel.
+type edsWatcher struct {
+	xdsClient xdsClientInterface
+	logger    *grpclog.PrefixLogger
+
+	updateChannel chan *watchUpdate
+
+	mu         sync.Mutex
+	edsToWatch string
+	edsCancel  func()
+}
+
+func (ew *edsWatcher) updateConfig(config *EDSConfig) {
+	// If EDSServiceName is set, use it to watch EDS. Otherwise, use the cluster
+	// name.
+	newEDSToWatch := config.EDSServiceName
+	if newEDSToWatch == "" {
+		newEDSToWatch = config.ClusterName
+	}
+
+	if ew.edsToWatch == newEDSToWatch {
+		return
+	}
+
+	// Restart EDS watch when the eds name to watch has changed.
+	ew.edsToWatch = newEDSToWatch
+
+	if ew.edsCancel != nil {
+		ew.edsCancel()
+	}
+	cancelEDSWatch := ew.xdsClient.WatchEndpoints(newEDSToWatch, func(update xdsclient.EndpointsUpdate, err error) {
+		// eb.updateCh.Put(&watchUpdate{eds: update, err: err})
+		select {
+		case <-ew.updateChannel:
+		default:
+		}
+		ew.updateChannel <- &watchUpdate{eds: update, err: err}
+	})
+	ew.logger.Infof("Watch started on resource name %v with xds-client %p", newEDSToWatch, ew.xdsClient)
+	ew.edsCancel = func() {
+		cancelEDSWatch()
+		ew.logger.Infof("Watch cancelled on resource name %v with xds-client %p", newEDSToWatch, ew.xdsClient)
+	}
+
+}
+
+// stopWatch stops the EDS watch.
+//
+// Call to updateConfig will restart the watch with the new name.
+func (ew *edsWatcher) stopWatch() {
+	if ew.edsCancel != nil {
+		ew.edsCancel()
+		ew.edsCancel = nil
+	}
+	ew.edsToWatch = ""
+}
